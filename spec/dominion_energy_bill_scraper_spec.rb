5d25170520b9b31d3d34261c447bd37742cf772f
Riley Shenk <rjshenk@gmail.com>
2019-05-21 23:35:54 -0400

    specs to covering the rest of DominionEnergyBillScraper

--- spec/dominion_energy_bill_scraper_spec.rb
+++ spec/dominion_energy_bill_scraper_spec.rb
@@ -17,5 +17,32 @@ describe DominionEnergyBillScraper do
     end
   end
 
-  # todo - stub out the input for TableScraper to watch how two tables are merged
+  describe '#collecting_data_from' do
+    let(:dominion_energy_scraper) { DominionEnergyBillScraper.new }
+    let(:usage_data) {{
+      'Meter Read Date' => ['Usage (kWh)'],
+      '04/24/2019' => ['670'],
+      '03/25/2019' => ['581']
+    }}
+    let(:tax_data) {{
+      'Date' => ['Bill Amount', 'Due Date'],
+      '04/24/2019' => ['$82.07','05/17/2019'],
+      '03/25/2019' => ['$72.50','04/18/2019']
+    }}
+
+
+    before do
+      allow(dominion_energy_scraper).to receive(:scrape).and_return(tax_data, usage_data)
+    end
+
+    it 'properly merges two hashes' do
+      merged_data = dominion_energy_scraper.collecting_data_from('', [], ['Date', 'Meter Read Date'])
+
+      expect(merged_data).to eq({
+        "Meter Read Date"=>["Bill Amount", "Due Date", "Usage (kWh)"],
+        "04/24/2019"=>["$82.07", "05/17/2019", "670"],
+        "03/25/2019"=>["$72.50", "04/18/2019", "581"]
+      })
+    end
+  end
 end
